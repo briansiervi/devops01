5c3150e0583f8a61f29f745c9d9dd3a07a42d587
briansiervi <brianmazini@gmail.com>
2025-05-19 08:56:48 -0300

    feat(nivel03_terraform): modularizando o s3

--- nivel_03/primeiro-projeto-iac/main.tf
+++ nivel_03/primeiro-projeto-iac/main.tf
@@ -1,9 +1,4 @@
-resource "aws_s3_bucket" "local_s3_bucket" {
-  bucket = "${var.org_name}-my-bucket-iac-${terraform.workspace}"
-
-  tags = {
-    Name    = "Primeiro bucket"
-    Iac     = true
-    Context = "${terraform.workspace}"
-  }
+module "s3" {
+  source         = "./modules/s3"
+  s3_bucket_name = "local-bucket-iac"
 }
--- nivel_03/primeiro-projeto-iac/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "org_name" {
-  type        = string
-  default     = "organization"
-  description = "description"
-}
