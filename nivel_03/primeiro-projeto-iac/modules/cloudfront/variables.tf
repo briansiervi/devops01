d6f1745713f6acd01b7c7c28b2e65a7c5c4a9168
briansiervi <brianmazini@gmail.com>
2025-05-20 08:55:12 -0300

    feat(nivel03_terraform): criando novas variáveis

--- nivel_03/primeiro-projeto-iac/modules/cloudfront/variables.tf
+++ nivel_03/primeiro-projeto-iac/modules/cloudfront/variables.tf
@@ -9,3 +9,15 @@ variable "bucket_domain_name" {
   default     = ""
   description = "Domínio do s3"
 }
+
+variable "cdn_price_class" {
+  type        = string
+  default     = "PriceClass_200"
+  description = "Classe de preço do CDN"
+}
+
+variable "cdn_tags" {
+  type        = map(string)
+  default     = {}
+  description = "Tags do CDN" 
+}
