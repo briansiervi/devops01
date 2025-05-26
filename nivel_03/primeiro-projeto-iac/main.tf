c3c260f27e21fd406ff1ec72a75999f0e7b3158b
briansiervi <brianmazini@gmail.com>
2025-05-26 08:50:45 -0300

    feat(nivel03_terraform): criando sqs + dlq

--- nivel_03/primeiro-projeto-iac/main.tf
+++ nivel_03/primeiro-projeto-iac/main.tf
@@ -6,16 +6,26 @@ module "s3" {
   }
 }
 
-module "cloudfront" {
-  source             = "./modules/cloudfront"
-  origin_id          = module.s3.bucket_id
-  bucket_domain_name = module.s3.bucket_domain_name
-  cdn_price_class    = "PriceClass_200"
-  cdn_tags = {
+# module "cloudfront" {
+#   source             = "./modules/cloudfront"
+#   origin_id          = module.s3.bucket_id
+#   bucket_domain_name = module.s3.bucket_domain_name
+#   cdn_price_class    = "PriceClass_200"
+#   cdn_tags = {
+#     Iac = true
+#   }
+
+#   depends_on = [
+#     module.s3
+#   ]
+# }
+
+module "sqs" {
+  source = "terraform-aws-modules/sqs/aws"
+  name   = "local-sqs"
+  create_dlq = true
+
+  tags = {
     Iac = true
   }
-
-  depends_on = [
-    module.s3
-  ]
 }
